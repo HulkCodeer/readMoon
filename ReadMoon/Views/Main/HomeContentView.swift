905100ff16add724a4c639b651e33767a0224e16
hyunjin.park <unicon85@gmail.com>
2022-03-25 20:49:19 +0900

    :construction: 메인화면 UI

--- ReadMoon/Views/Main/HomeContentView.swift
+++ ReadMoon/Views/Main/HomeContentView.swift
@@ -10,16 +10,50 @@ import SwiftUI
 struct HomeContentView: View {
     var body: some View {
         VStack {
-            HStack {
+            HStack(spacing: 0) {
                 Image("calendarIcon")
-                    .frame(width: 32, height: 32)                
+                    .frame(width: 32, height: 32)
+                
                 Text("캘린더")
                     .font(Font.system(size: 24, weight: .bold))
+                    .padding(.leading, 6)
+                    .padding(.trailing, 8)
+                
                 Image("calendarGuideIcon")
                     .frame(width: 24, height: 24)
+                
                 Spacer()
             }
+            .frame(width: .infinity, height: 60, alignment: .center)
+            .padding(.leading, 20)
             
+            HStack {
+                Button(action: {
+                    
+                }, label: {
+                    Image("calendarLeftArrow")
+                })
+                .frame(width: 30, height: 30)
+                
+                Spacer()
+                
+                Text("2021년 10월")
+                    .font(Font.system(size: 20, weight: .bold))
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image("calendarRightArrow")
+                })
+                .frame(width: 30, height: 30)
+            }
+            .frame(width: .infinity, height: 60, alignment: .center)
+            .padding(.leading, 20)
+            .padding(.trailing, 20)
+            
+            Spacer()
         }
     }
 }
